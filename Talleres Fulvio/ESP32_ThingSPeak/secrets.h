fb0dd53484a9b35b246fa769cd35bd9d948e92e8
Elmer Jose Muñoz Zuñiga <67802793+Ejmz216@users.noreply.github.com>
2021-09-12 21:39:26 -0500

    First Commit

---
+++ Talleres Fulvio/ESP32_ThingSPeak/secrets.h
@@ -0,0 +1,8 @@
+///Use this file to store all of the private credentials 
+/// and connection details
+
+#define SECRET_SSID "MUNOZ ZUNIGA"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "25181226"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1373670     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "DE4SNMVKH80ORCWI"   // replace XYZ with your channel write API Key
